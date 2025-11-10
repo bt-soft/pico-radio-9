4576aa6b8eec16958f537d7b85123720816e07f2
bt-soft <bt-soft@freemail.hu>
2025-11-10 22:00:33 +0100

    Improve comment clarity for CLEAR_AREA_WIDTH constant in UICompSevenSegmentFreq

--- src/UICompSevenSegmentFreq.cpp
+++ src/UICompSevenSegmentFreq.cpp
@@ -15,7 +15,8 @@ constexpr static int CHAR_WIDTH_DOT = 3;    // '.' karakter szélessége
 constexpr static int CHAR_WIDTH_SPACE = 1;  // ' ' karakter szélessége
 constexpr static int CHAR_WIDTH_DASH = 23;  // '-' karakter szélessége
 
-constexpr static uint16_t CLEAR_AREA_WIDTH = 250; // Törlési terület szélessége (frekvencia + mértékegység)
+// Törlési terület szélessége (frekvencia + mértékegység), pl.: a BFO animáció előtt és után
+constexpr static uint16_t CLEAR_AREA_WIDTH = 250;
 
 /**
  * @brief UICompSevenSegmentFreq konstruktor - inicializálja a frekvencia kijelző komponenst
