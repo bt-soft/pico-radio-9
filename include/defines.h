114d451dbe3f831b9fcd08d6b1753dc24baf44f7
bt-soft <bt-soft@freemail.hu>
2025-11-12 20:18:20 +0100

    Disable waiting for serial in debug mode by commenting out DEBUG_WAIT_FOR_SERIAL

--- include/defines.h
+++ include/defines.h
@@ -37,7 +37,7 @@
 #define __DEBUG // Debug mód vezérlése
 
 // Soros portra várakozás az induláskor debug módban
-#define DEBUG_WAIT_FOR_SERIAL
+// #define DEBUG_WAIT_FOR_SERIAL
 
 // #define SHOW_MEMORY_INFO               // Memória monitor bekapcsolása memory leak nyomon követésére
 // #define MEMORY_INFO_INTERVAL 20 * 1000 // 20mp
