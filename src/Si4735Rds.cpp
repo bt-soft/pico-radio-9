dd543a06eee2ebb66e3a6bf42cd0d72bf9f8fd13
bt-soft <bt-soft@freemail.hu>
2025-12-19 16:34:23 +0100

    Update last modified timestamp and disable Si4735RDS debug mode

--- src/Si4735Rds.cpp
+++ src/Si4735Rds.cpp
@@ -14,7 +14,7 @@
  * 	Egyetlen feltétel:                                                                                                 *
  * 		a licencet és a szerző nevét meg kell tartani a forrásban!                                                     *
  * -----                                                                                                               *
- * Last Modified: 2025.11.16, Sunday  08:27:46                                                                         *
+ * Last Modified: 2025.12.19, Friday  04:24:47                                                                         *
  * Modified By: BT-Soft                                                                                                *
  * -----                                                                                                               *
  * HISTORY:                                                                                                            *
@@ -27,7 +27,7 @@
 #include "StationData.h"
 
 // Si4735RDS működés debug engedélyezése de csak DEBUG módban
-#define __SI4735RDS_DEBUG
+// #define __SI4735RDS_DEBUG
 #if defined(__DEBUG) && defined(__SI4735RDS_DEBUG)
 #define SI4735RDS_DEBUG(fmt, ...) DEBUG(fmt __VA_OPT__(, ) __VA_ARGS__)
 #else
